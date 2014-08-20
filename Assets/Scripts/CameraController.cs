6cf04b5f9cef8da31ef53e3c1fea3ce08ff5b445
tjadams <robocustom@hotmail.com>
2014-08-20 16:14:02 -0400

    scraped movement and camera necessities from roll-a-ball. Started coding an input joystick for use on Android.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// NOTE: public variables can be accessed in the Inspector which allows for quick edits to the variable without recompiling
+	public GameObject player;
+
+	// this value will be used by the camera to follow the player's position
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	// called before rendering a frame, game code goes here
+	void Update ()
+	{
+
+	}
+
+	// called before a physics action, physics code goes here
+	public float speed;
+
+	void FixedUpdate () 
+	{
+		//TODO change this input stuff to work on Android
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+}
