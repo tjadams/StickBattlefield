cd241808b76d52aa3ac3772184c8b21a85d509b3
tjadams <robocustom@hotmail.com>
2014-08-21 10:57:13 -0400

    pivoted to "Balls with Guns" 4 minutes before presentation :)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour 
-{
-	// called before rendering a frame, game code goes here
-	void Update ()
+public class PlayerController : TouchLogic {
+	
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	void OnTouchBegan()
 	{
-
-	}
-
-	// called before a physics action, physics code goes here
-	public float speed;
-
-	void FixedUpdate () 
-	{
-		//TODO change this input stuff to work on Android
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-
-
-		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
-
-		rigidbody.AddForce(movement * speed * Time.deltaTime);
+		Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
 	}
 }
